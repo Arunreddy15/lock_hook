7a33bde313e69d86a3b22d33a4d15fdd807641f8
Arunreddy <60958077+Aunreddy15@users.noreply.github.com>
2022-03-20 11:38:03 +0530

    Update styledComponents.js

--- src/components/Unlock/styledComponents.js
+++ src/components/Unlock/styledComponents.js
@@ -10,5 +10,13 @@ export const MainContainer = styled.div`
 `
 export const ImagesContainer = styled.div``
 export const ImgTag = styled.img``
-export const Text = styled.p``
-export const Button = styled.button``
+export const Text = styled.p`
+font-family:"Roboto";
+color:White;
+font-size:24px;
+`
+export const Button = styled.button`
+border-radius:8px;
+padding:5px 10px;
+border:0px;
+`
